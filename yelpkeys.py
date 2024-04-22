9d7d500ec4860e3e941c5b10f8e66dc6ccb616ab
DanaTryon <157637222+DanaTryon@users.noreply.github.com>
2024-04-22 14:27:00 -0400

    Initial commit

---
+++ yelpkeys.py
@@ -0,0 +1,10 @@
+#  -*- coding: utf-8 -*-
+"""
+Created 4/19/2024
+@author: Dana
+API keys for Yelp Fusion API for review sentiment App
+"""
+
+
+client_id="U2BWtPqeO9KQ3pbo6I2nw"
+api_key="P46abUcC8UKX2Wpy0Gjje63aaFrlodvPxM7PJ2IWpBCiZgG-xgVSzvpsmt8ozd4aY2k4EWvYRwD546ZZGANUG_qUrCd14SH5aAHAz-JIok-tlYItJZLxqYudIK8hZnYx"
